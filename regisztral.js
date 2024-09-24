5419f6fd6c0577ffec84cb7c64e5112f92d62d80
Bernát <bernatoliver04@gmail.com>
2024-09-24 09:31:22 +0200

    Html css kész, jsen dolgozni kell

--- regisztral.js
+++ regisztral.js
@@ -1,5 +1,67 @@
-function AutoGenerate(){} //password autógenerálás
+function AutoGenerate(){//password autógenerálás
+    let jelszo = "";
+    for (let i = 0; i < 10; i++) {
+        Math.floor(Math.random() * 10);
+        //vm algoritmust
+    }
 
-function AgeSliderChange(){} //age kiválasztás
+    
+}
 
-function NemKivalaszt(nem){}//nem kiválaszt bordert adni h tudjuk melyik lett
+function ShowPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+function AgeSliderChange(){//age kiválasztás
+    document.getElementById("ageLabel").innerText = "Életkor: " + document.getElementById("age").value;
+}
+
+function NemKivalaszt(nem){ //nem kiválasztása
+    if (arguments.length == 0) {
+        if (document.getElementById('male').classList.contains("genderSelect")) {
+            return "male";
+        }
+        return "female";
+    }
+
+    if (nem == "male") {
+        document.getElementById("male").classList.add("genderSelect");
+        document.getElementById("female").classList.remove("genderSelect");
+        return;
+    }
+
+    document.getElementById("male").classList.remove("genderSelect");
+    document.getElementById("female").classList.add("genderSelect");
+    return nem;
+}
+
+function Submit() {
+    let nev = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let gender = NemKivalaszt();
+    let job = document.getElementById("job").value;
+    let fields = document.getElementById("fields").value;
+    let password = document.getElementById("password").value;
+
+    if (nev == "" || email == "" || job == "" || password == "") {
+        alert("Minden megadott mezőt ki kell tölteni!");
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        alert("Nem megfelelő az email formátuma");
+        return;
+    }
+    
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
